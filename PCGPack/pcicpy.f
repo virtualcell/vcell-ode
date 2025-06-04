33380c843bfdada00310fe207a2f7523a0cb3ce6
Logan Drescher <drescher@uchc.edu>
2025-06-04 14:21:35 -0400

    purge round three

--- PCGPack/pcicpy.f
+++
@@ -1,9 +0,0 @@
-      SUBROUTINEPCICPY(N,I,J)
-      IMPLICITDOUBLEPRECISION(A-H,O-Z)
-      INTEGERN,I(*),J(*)
-      INTEGERK
-      DO1K=1,N
-      J(K)=I(K)
-1     CONTINUE
-      RETURN
-      END
