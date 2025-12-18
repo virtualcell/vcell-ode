baf7586fd65a6a7b7633f4e8567e41e26b1be5cb
Logan Drescher <drescher@uchc.edu>
2025-12-18 13:05:52 -0500

    removed int-overflow in test

--- tests/unit/message_processing_test.cpp
+++ tests/unit/message_processing_test.cpp
@@ -13,13 +13,13 @@ static std::vector<std::string> eventTracker;
 
 void appendNextIndex(const WorkerEvent* event);
 std::vector<std::string> generateFibonacci(int length);
-long sequenceComparison(const std::vector<std::string> &sequence1, const std::vector<std::string> &sequence2);
+long long sequenceComparison(const std::vector<std::string> &sequence1, const std::vector<std::string> &sequence2);
 
 
 TEST(MessageProcessingTest, MessagesAreProcessed) {
 	try {
 		MessageEventManager eventManager{[](WorkerEvent* event)->void{appendNextIndex(event);}};
-		const int NUM_ITERATIONS = 100;
+		const int NUM_ITERATIONS = 50;
 		for (int i = 0; i < NUM_ITERATIONS; ++i) {
 			JobEvent::Status status;
 			switch (i + 1) {
@@ -45,8 +45,8 @@ void appendNextIndex(const WorkerEvent* event) {
 		} else if (1 == eventTracker.size()) {
 			eventTracker.emplace_back("1");
 		} else {
-			const long firstValue = std::stol(eventTracker[eventTracker.size() - 2]);
-			const long secondValue = std::stol(eventTracker[eventTracker.size() - 1]);
+			const long long firstValue = std::stoll(eventTracker[eventTracker.size() - 2]);
+			const long long secondValue = std::stoll(eventTracker[eventTracker.size() - 1]);
 			const std::string nextValue{std::to_string(firstValue + secondValue)};
 			eventTracker.push_back(nextValue);
 		}
@@ -61,20 +61,20 @@ std::vector<std::string> generateFibonacci(const int length) {
 	for (int i = 0; i < length; ++i) {
 		if (i == 0) { sequence.emplace_back("0"); continue; }
 		if (i == 1) { sequence.emplace_back("1"); continue; }
-		const long firstValue = std::stol(sequence[sequence.size() - 2]);
-		const long secondValue = std::stol(sequence[sequence.size() - 1]);
+		const long long firstValue = std::stoll(sequence[sequence.size() - 2]);
+		const long long secondValue = std::stoll(sequence[sequence.size() - 1]);
 		const std::string nextValue{std::to_string(firstValue + secondValue)};
 		sequence.push_back(nextValue);
 	}
 	return sequence;
 }
 
-long sequenceComparison(const std::vector<std::string> &sequence1, const std::vector<std::string> &sequence2) {
+long long sequenceComparison(const std::vector<std::string> &sequence1, const std::vector<std::string> &sequence2) {
 	if (const size_t comp = sequence2.size() - sequence1.size(); comp) return static_cast<int>(comp);
 	for (size_t i = 0; i < sequence1.size(); ++i) {
-		const long firstValue = std::stol(sequence1[i]);
-		const long secondValue = std::stol(sequence2[i]);
-		if (const long comp = secondValue - firstValue; comp) return comp;
+		const long long firstValue = std::stoll(sequence1[i]);
+		const long long secondValue = std::stoll(sequence2[i]);
+		if (const long long comp = secondValue - firstValue; comp) return comp;
 	}
 	return 0;
 }
